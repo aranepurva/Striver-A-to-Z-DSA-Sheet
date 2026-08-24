ce2b507b81a5e0dfb10a8f0948b180844f386601
aranepurva <aranepurva@gmail.com>
2026-08-24 22:43:22 +0530

    Implement pattern printing based on user input
    Added a pattern printing program that takes user input.

--- 02_Patterns/Pattern06.java
+++ 02_Patterns/Pattern06.java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+class Pattern06{
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int n=Sc.nextInt();
+        
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+            System.out.print(j);
+            }
+            System.out.println();
+        }
+        Sc.close();
+    }
+}
