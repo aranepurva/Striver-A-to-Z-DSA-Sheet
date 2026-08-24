f0a2283c5100992189863b555502de20c03faa77
aranepurva <aranepurva@gmail.com>
2026-08-24 22:27:04 +0530

    Implement number pattern printing in Pattern03.java
    Added a pattern printing program that outputs numbers in increasing order based on user input.

--- 02_Patterns/Pattern03.java
+++ 02_Patterns/Pattern03.java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+class Pattern03{
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int n=Sc.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+            System.out.print(j);
+            }
+            System.out.println();
+        }
+        Sc.close();
+    }
+}
