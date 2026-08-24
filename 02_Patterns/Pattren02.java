4381623db31b128a1016ebdbe32e22259915ce34
aranepurva <aranepurva@gmail.com>
2026-08-24 22:20:45 +0530

    Implement pattern printing in Pattren02.java
    Added a pattern printing program that takes user input.

--- 02_Patterns/Pattren02.java
+++ 02_Patterns/Pattren02.java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+class Pattren02{
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int n=Sc.nextInt();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+        Sc.close();
+    }
+}
