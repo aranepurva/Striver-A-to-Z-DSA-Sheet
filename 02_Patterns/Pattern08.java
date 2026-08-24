68aed37746b75f0e59de573d346c51055daec2de
aranepurva <aranepurva@gmail.com>
2026-08-24 23:06:58 +0530

    Implement pyramid pattern printing in Pattern08.java
    Added a Java program to print a pyramid pattern based on user input.

--- 02_Patterns/Pattern08.java
+++ 02_Patterns/Pattern08.java
@@ -1,1 +1,21 @@
-
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int n=Sc.nextInt();
+        
+        for(int i=0;i<n;i++){
+            //space
+            for(int j=0;j<i;j++){
+            System.out.print(" ");
+            }
+            //star
+            for(int j=0;j<2*(n-i)-1;j++){
+            System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+        Sc.close();
+    }
+}
