05a360306ca007a42eb9e027b98c0d3e0c840744
aranepurva <aranepurva@gmail.com>
2026-08-24 22:40:34 +0530

    Correct pattern printing logic in Pattern05.java
    Fixed loop condition and added missing increment for inner loop.

--- 02_Patterns/Pattern05.java
+++ 02_Patterns/Pattern05.java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+class Pattern05 {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int n=Sc.nextInt();
+        
+        for(int i=n;i>=0;i--){
+            for(int j=0;j<=i;j++){
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+        Sc.close();
+    }
+}
